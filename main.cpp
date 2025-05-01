158fab647c85640e03003e2fee319c6f09578f5d
Daniel Ângelo <danielangelomr@gmail.com>
2025-05-01 14:10:06 -0300

    Atualização README e conceitos de

--- main.cpp
+++ main.cpp
@@ -5,48 +5,9 @@ struct Disciplina
 {
     double notas[4], somaMedia, media;
 
-    void setNota()
-    {
-        int posicaoNota;
-
-        std::cout<<"Qual nota deseja modificar? (1 a 4)"<<std::endl;
-        std::cin>>posicaoNota;
-
-        if(posicaoNota >= 1 && posicaoNota <= 4)
-        {
-            std::cout<<"Digite a nota "<<posicaoNota<<":"<<std::endl;
-            std::cin>>notas[posicaoNota-1];
-            
-            if(notas[posicaoNota-1] >= 0 && notas[posicaoNota-1] <= 10)
-            {
-            std::cout<<"Nota adicionada com sucesso!"<<std::endl;
-            } else
-            {
-                std::cout<<"Erro! Digite a nota de 0 a 10!"<<std::endl;
-                return setNota();
-            }
-            
-        } else
-        {
-            std::cout<<"Erro! Escolha a nota de 1 a 4!"<<std::endl;
-            return setNota();
-        }  
-    }
-    void exibirNotas(){
-        for (int i = 0; i < 4; i++)
-        {
-            std::cout<<"Nota "<<i+1<<" "<<notas[i]<<std::endl;
-        }
-        
-    }
-    void calcularMedia()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            somaMedia += notas[i];
-        }
-    notas[5] = somaMedia / 4;
-    }
+    void setNota();
+    void exibirNotas();
+    void calcularMedia();
 };
 
 struct Aluno
@@ -55,47 +16,12 @@ struct Aluno
     int idade;
     Disciplina disciplinas[5];
 
-    void setNome(){
-        std::cout<<"Digite o nome:"<<std::endl;
-        std::cin>>nome;
-    }
-    void setIdade(){
-        std::cout<<"Digite a idade de "<<nome<<":"<<std::endl;
-        std::cin>>idade;
-    }
-    void setAluno(){
-        std::cout<<"----Adicionar aluno----"<<std::endl;
-        setNome();
-        setIdade();
-    }
-    void setNotaAluno(){
-        int i;
-        std::cout<<"Qual disciplina deseja modificar nota: (1 a 5)"<<std::endl;
-        std::cin>>i;
-        if (i >= 1 && i <= 5)
-        {
-            disciplinas[i-1].setNota();
-        }else{
-            std::cout<<"Erro disciplina errada, digite valor de 1 a 5"<<std::endl;
-            return setNotaAluno();
-        }
-        
-        
-    }
-    void exibirDisciplinasAluno(){
-        for (int i = 0; i < 5; i++)
-        {
-            std::cout<<"Disciplina "<<i+1<<":"<<std::endl;
-            disciplinas[i].exibirNotas();
-        }
-        
-    }
-    
-    void exibirAluno(){
-        std::cout<<"Nome: "<<nome<<"."<<std::endl;
-        std::cout<<"Idade: "<<idade<<"."<<std::endl;
-        exibirDisciplinasAluno();
-    }
+    void setNome();
+    void setIdade();
+    void setAluno();
+    void setNotaAluno();
+    void exibirDisciplinasAluno();
+    void exibirAluno();
 };
 
 void exibirAlunos(Aluno alunos[], int alunosRegistrados){
@@ -111,7 +37,7 @@ void exibirAlunos(Aluno alunos[], int alunosRegistrados){
 int main(int argc, char const *argv[])
 {
     setlocale(LC_ALL, "pt_BR.UTF-8");
-
+    
     const int TOTAL_ALUNOS = 50;
     Aluno alunos[TOTAL_ALUNOS];
     int alunosRegistrados = 0;
@@ -173,3 +99,104 @@ int main(int argc, char const *argv[])
         }
     } while (flag == 1);
 }
+#pragma region Disciplina
+
+void Disciplina::setNota()
+{
+    int posicaoNota;
+
+    std::cout<<"Qual nota deseja modificar? (1 a 4)"<<std::endl;
+    std::cin>>posicaoNota;
+
+    if(posicaoNota >= 1 && posicaoNota <= 4)
+    {
+        std::cout<<"Digite a nota "<<posicaoNota<<":"<<std::endl;
+        std::cin>>notas[posicaoNota-1];
+        
+        if(notas[posicaoNota-1] >= 0 && notas[posicaoNota-1] <= 10)
+        {
+        std::cout<<"Nota adicionada com sucesso!"<<std::endl;
+        } else
+        {
+            std::cout<<"Erro! Digite a nota de 0 a 10!"<<std::endl;
+            return setNota();
+        }
+        
+    } else
+    {
+        std::cout<<"Erro! Escolha a nota de 1 a 4!"<<std::endl;
+        return setNota();
+    }  
+}
+
+void Disciplina::exibirNotas()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        std::cout<<"Nota "<<i+1<<" "<<notas[i]<<std::endl;
+    }
+}
+
+void Disciplina::calcularMedia()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        somaMedia += notas[i];
+    }
+    notas[5] = somaMedia / 4;
+}
+
+#pragma endregion
+
+#pragma region Aluno
+
+void Aluno::setNome()
+{
+    std::cout<<"Digite o nome:"<<std::endl;
+    std::cin>>nome;
+}
+
+void Aluno::setIdade()
+{
+    std::cout<<"Digite a idade de "<<nome<<":"<<std::endl;
+    std::cin>>idade;
+}
+
+void Aluno::setAluno()
+{
+    std::cout<<"----Adicionar aluno----"<<std::endl;
+    setNome();
+    setIdade();
+}
+
+void Aluno::setNotaAluno()
+{
+    int i;
+    std::cout<<"Qual disciplina deseja modificar nota: (1 a 5)"<<std::endl;
+    std::cin>>i;
+    if (i >= 1 && i <= 5)
+    {
+        disciplinas[i-1].setNota();
+    }else{
+        std::cout<<"Erro disciplina errada, digite valor de 1 a 5"<<std::endl;
+        return setNotaAluno();
+    }
+}
+
+void Aluno::exibirDisciplinasAluno()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout<<"Disciplina "<<i+1<<":"<<std::endl;
+        disciplinas[i].exibirNotas();
+    }
+}
+
+void Aluno::exibirAluno()
+{
+    std::cout<<"Nome: "<<nome<<"."<<std::endl;
+    std::cout<<"Idade: "<<idade<<"."<<std::endl;
+    exibirDisciplinasAluno();
+}
+
+#pragma endregion
